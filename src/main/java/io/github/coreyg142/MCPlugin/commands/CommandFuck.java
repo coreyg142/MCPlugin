835f1079da5bb58caea7c01d79597cbfb5350527
Corey <coreyg142@gmail.com>
2021-04-10 11:28:23 -0400

    Added a chat message to send to the calling Player

--- src/main/java/io/github/coreyg142/MCPlugin/commands/CommandFuck.java
+++ src/main/java/io/github/coreyg142/MCPlugin/commands/CommandFuck.java
@@ -1,5 +1,6 @@
 package io.github.coreyg142.MCPlugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,11 +8,25 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The command executor for the /fuck command
+ * Gives the player a diamond with god enchantments.
+ *
+ * @author Corey Gross
+ */
 public class CommandFuck implements CommandExecutor {
+	/**The message to send the player after execution*/
+	private final static String MESSAGE = (ChatColor.DARK_PURPLE + "" + ChatColor.BOLD +
+			"You have been blessed with The Diamond");
+	/**The name of the Diamond*/
+	private final static String DIAMOND_NAME = (ChatColor.BOLD + "" + ChatColor.AQUA + "" + ChatColor.MAGIC + "asd" +
+			ChatColor.RESET + ChatColor.BOLD + ChatColor.LIGHT_PURPLE + "The Diamond" +
+			ChatColor.RESET + ChatColor.BOLD + ChatColor.AQUA + ChatColor.MAGIC + "asd");
 
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -19,11 +34,15 @@ public class CommandFuck implements CommandExecutor {
 			Player player = (Player) commandSender;
 
 			ItemStack diamond = new ItemStack(Material.DIAMOND);
-			ItemStack bricks = new ItemStack(Material.BRICK, 20);
 
-			diamond.addEnchantments(getGodEnchants());
+			diamond.addUnsafeEnchantments(getGodEnchants());
+			ItemMeta diamondMeta = diamond.getItemMeta();
+			assert diamondMeta != null;
+			diamondMeta.setDisplayName(DIAMOND_NAME);
 
-			player.getInventory().addItem(diamond, bricks);
+			diamond.setItemMeta(diamondMeta);
+			player.getInventory().addItem(diamond);
+			player.sendMessage(MESSAGE);
 		}
 
 		return true;
@@ -34,9 +53,8 @@ public class CommandFuck implements CommandExecutor {
 
 		enchantments.put(Enchantment.DAMAGE_ALL, 5000);
 		enchantments.put(Enchantment.DURABILITY, 5000);
-		enchantments.put(Enchantment.DIG_SPEED, 5000);
-		enchantments.put(Enchantment.LOOT_BONUS_BLOCKS, 5000);
 		enchantments.put(Enchantment.LOOT_BONUS_MOBS, 5000);
+		enchantments.put(Enchantment.FIRE_ASPECT, 5000);
 
 		return enchantments;
 	}
