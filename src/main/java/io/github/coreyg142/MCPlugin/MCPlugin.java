ce5ca6f66120e0f15773f83c1e6db311f21215b6
Corey <coreyg142@gmail.com>
2021-04-10 09:38:59 -0400

    Initial build state

--- src/main/java/io/github/coreyg142/MCPlugin/MCPlugin.java
+++ src/main/java/io/github/coreyg142/MCPlugin/MCPlugin.java
@@ -4,4 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCPlugin extends JavaPlugin {
 
+	@Override
+	public void onEnable(){
+		getLogger().info("onEnable is called!");
+	}
+
+	@Override
+	public void onDisable(){
+		getLogger().info("onDisable is called!");
+	}
 }
